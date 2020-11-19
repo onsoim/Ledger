b5795f2287429434ad70e6570891553855495951
onsoim <onsoim@gmail.com>
2020-11-19 16:53:48 +0900

    Automated version update
    I've added automated version updating code to build.gradle. And it can updating version automatically.

--- app/src/main/java/com/onsoim/ledger/MainActivity.kt
+++ app/src/main/java/com/onsoim/ledger/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.onsoim.ledger
 
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text.text = GetAppVersion(this)
+    }
+
+    fun GetAppVersion(context: Context): String {
+        var version = ""
+
+        try {
+            val pInfo = context.packageManager.getPackageInfo(context.packageName, 0)
+            version = pInfo.versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+
+        return version
     }
 }
